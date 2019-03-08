4032578ed83fe09993695e7220b5c49f58e0aff1
becomingadarsha <adarshaofficial@gmail.com>
2019-03-08 21:56:22 +0545

    comment added

--- Module 6/challenge.js
+++ Module 6/challenge.js
@@ -3,8 +3,8 @@ let favChannels = ['Mr. Beast', 'Traversy Media', 'Tech Lead', 'CS50', 'CS Dojo'
   'KSI', 'Flying Beast', 'Be YouNick', 'The Viral Fever', 'Gaurav Sen', 'Rachit Jain', 'The Food Ranger', 'Casey Neistat'
 ];
 
-favChannels.unshift('Pewdiepie');
-favChannels.pop();
+favChannels.unshift('Pewdiepie'); // Add Pewdiepie at first of array
+favChannels.pop(); // Remove last element of array
 
 //Printing elements in array
 for (let i = 0; i < favChannels.length; i++) {
