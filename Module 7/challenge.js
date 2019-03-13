d1daf7b4926e5f7c2a9b5e36103e13b01a2228dd
becomingadarsha <adarshaofficial@gmail.com>
2019-03-13 10:16:33 +0545

    Objects in JS

---
+++ Module 7/challenge.js
@@ -0,0 +1,25 @@
+// creating object of my name
+
+let aadarsha = {
+  name: "Aadarsha Acharya",
+  age: 19,
+  address: "Naikap, Kathmandu",
+  college: "Asian College of Higher Studies",
+  degree: "Bsc. CSIT",
+  semester: "Third",
+  nationality: "Nepali",
+  height: " 5' 9\" "
+};
+
+console.log(aadarsha);
+
+// smartphone object`
+let smartphone = {
+  model: "Google Pixel 3 XL",
+  os: "Android 9.0",
+  screensize: "6.30 inches",
+  ram: "4 GB",
+  batterycapacity: "3420 mAh"
+};
+
+console.log(Object.keys(smartphone));
